8acc0cf75245879f35f3429f290ee0f69fef0b9a
smriti-adl <smriti.singh.adl@gmail.com>
2025-05-30 16:28:25 +0930

    remove target group

--- main.tf
+++ main.tf
@@ -43,8 +43,6 @@ module "autoscaling" {
   max_size=2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns = [aws_lb_target_group.blog.arn]
-
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id    = data.aws_ami.app_ami.id
