cc4f2ca089174434127179c9f3459ee6d3ce9a36
smriti-adl <smriti.singh.adl@gmail.com>
2025-05-30 16:20:35 +0930

    autoscaling

--- main.tf
+++ main.tf
@@ -35,16 +35,21 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.3.0"
+  name    ="blog"
+  min_size=1
+  max_size=2
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_abl.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
 
-  tags   = {
-    Name = "HelloWorld"
-  }
+  amiimage_id    = data.aws_ami.app_ami.id
+  instance_type  = var.instance_type
+
+  # insert the 1 required variable here
 }
 
 module "alb" {
@@ -53,7 +58,7 @@ module "alb" {
   name    = "blog-alb"
   vpc_id  = "module.blog_vpc.vpc_id"
   subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
   # Security Group
   security_group_ingress_rules = {
